c3d7146ef4c4848979649f9263998155a041e988
Hugo <hugo@srv1549931.hstgr.cloud>
2026-07-22 13:46:48 +0000

    Add canonical host redirects

---
+++ functions/_middleware.js
@@ -0,0 +1,18 @@
+const CANONICAL_HOST = "basementwaterproofingchattanooga.com";
+const REDIRECT_HOSTS = new Set([CANONICAL_HOST, `www.${CANONICAL_HOST}`]);
+
+export function onRequest(context) {
+  const url = new URL(context.request.url);
+
+  if (!REDIRECT_HOSTS.has(url.hostname)) {
+    return context.next();
+  }
+
+  if (url.protocol === "https:" && url.hostname === CANONICAL_HOST) {
+    return context.next();
+  }
+
+  url.protocol = "https:";
+  url.hostname = CANONICAL_HOST;
+  return Response.redirect(url.toString(), 301);
+}
